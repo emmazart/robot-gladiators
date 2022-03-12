ad64203ffb4b2d5139947c4f210b15288045d3c0
Emma Olson <emma.olson794@gmail.com>
2022-03-12 17:12:11 -0600

    beginning js file

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-window.alert("This is an alert! JavaScript is running!");
+// this declares playerName variable
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// this combines a string (our robots name is) with a variable
+// string concatenation
+console.log("Our robot's name is " + playerName);
+
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
